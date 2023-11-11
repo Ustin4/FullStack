6effa97d041de29ab417fe0b878d4785577fbd1f
Bubchao <mityaborisenok@mail.ru>
2023-11-12 02:40:48 +0300

    full add with no auth

--- store/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from backend_api.views import *
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', ProductsView.as_view(), name='govno'),
-]
