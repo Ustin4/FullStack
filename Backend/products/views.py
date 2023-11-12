2fcbd1e8107eeb8977ed62f0dded0636b73b5824
Bubchao <mityaborisenok@mail.ru>
2023-11-13 01:15:45 +0300

    dict to list

--- Backend/products/views.py
+++ Backend/products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+
 from .models import Device, DeviceBrand, DeviceType
 from .serializers import DeviceSerializer, DeviceTypeSerializer, DeviceBrandSerializer
 
@@ -7,6 +9,11 @@ from .serializers import DeviceSerializer, DeviceTypeSerializer, DeviceBrandSeri
 class DeviceListCreateView(generics.ListCreateAPIView):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data[0])
 
 
 class DeviceRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
@@ -18,6 +25,11 @@ class DeviceBrandListCreateView(generics.ListCreateAPIView):
     queryset = DeviceBrand.objects.all()
     serializer_class = DeviceBrandSerializer
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data[0])
+
 
 class DeviceBrandRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = DeviceBrand.objects.all()
@@ -27,6 +39,11 @@ class DeviceBrandRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView
 class DeviceTypeListCreateView(generics.ListCreateAPIView):
     queryset = DeviceType.objects.all()
     serializer_class = DeviceTypeSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data[0])
 
 
 class DeviceTypeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
